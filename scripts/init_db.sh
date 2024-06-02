deeb9a3f8184e1cbbc3c8c0829eb435c2aad306b
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-06-02 13:11:54 +0900

    build: change local database details

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -15,16 +15,11 @@ if ! [ -x "$(command -v sqlx)" ]; then
   exit 1
 fi
 
-# Check if a custom user has been set, otherwise default to 'postgres'
-DB_USER="${POSTGRES_USER:=postgres}"
-# Check if a custom password has been set, otherwise default to 'password'
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom database name has been set, otherwise default to 'delivr'
-DB_NAME="${POSTGRES_DB:=delivr}"
-# Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
-# Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
+DB_USER="${POSTGRES_USER:=delivr_user}"
+DB_NAME="${POSTGRES_DB:=delivr_database}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_PORT="${POSTGRES_PORT:=5432}"
 
 # Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
