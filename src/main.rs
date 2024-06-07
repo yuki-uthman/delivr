ea76d272e47599a45455b4f3dd5cf42d689d22e4
Yuki Yoshimine <yuki07yuki@gmail.com>
2024-06-07 21:52:16 +0900

    refactor: remove time and color on render deploy

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,42 @@
-use delivr::app;
 use delivr::config::get_config;
+use delivr::{app, config::Environment};
 use tracing::subscriber::set_global_default;
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(tracing_subscriber::fmt::layer());
-
-    set_global_default(subscriber).unwrap();
-
     // check app environment
     if std::env::var("APP_ENVIRONMENT").is_err() {
         std::env::set_var("APP_ENVIRONMENT", "local");
     }
 
     let config = get_config()?;
-    tracing::info!("{:#?}", config);
 
+    LogTracer::init().expect("Failed to set logger");
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
+    match config.environment {
+        Environment::Production => {
+            let subscriber = Registry::default().with(env_filter).with(
+                tracing_subscriber::fmt::layer()
+                    .with_ansi(false)
+                    .without_time(),
+            );
+
+            set_global_default(subscriber).unwrap();
+        }
+        _ => {
+            let subscriber = Registry::default()
+                .with(env_filter)
+                .with(tracing_subscriber::fmt::layer().with_ansi(true));
+
+            set_global_default(subscriber).unwrap();
+        }
+    }
+
+    tracing::info!("{:#?}", config);
     app::serve(&config).await?;
 
     Ok(())
